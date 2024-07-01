95adbda317abe651be8b9e5226e70e85e22c6a34
Karsei <karseidev@gmail.com>
2024-07-01 13:01:26 +0900

    feat: 프로젝트 구성

---
+++ src/main/java/kr/pe/karsei/tobyspring6inflearn/Tobyspring6InflearnApplication.java
@@ -0,0 +1,11 @@
+package kr.pe.karsei.tobyspring6inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tobyspring6InflearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Tobyspring6InflearnApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/pe/karsei/tobyspring6inflearn/Tobyspring6InflearnApplicationTests.java
@@ -0,0 +1,11 @@
+package kr.pe.karsei.tobyspring6inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tobyspring6InflearnApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
